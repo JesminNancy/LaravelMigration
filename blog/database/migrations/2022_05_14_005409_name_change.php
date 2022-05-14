68715892a0d463d46e2e87af5185c07e20ed92c1
JesminNancy <jesminnancy@hotmail.com>
2022-05-14 10:24:43 +0600

    Creating & Modifying Columns (Migration Up)

---
+++ blog/database/migrations/2022_05_14_005409_name_change.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('all_table', function(Blueprint $table){
+            
+            $table->renameColumn('city', 'my_city');
+            
+        });
+        
+        Schema::table('students', function(Blueprint $table){
+            
+            $table->renameColumn('name', 'students_name');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
